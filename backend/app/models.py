b4cda5fe7c4bdd054995aafa3746836a5e1c2856
zilloone <jsaudia1212@gmail.com>
2025-05-28 11:41:44 +0100

    Initial Project setup

---
+++ backend/app/models.py
@@ -0,0 +1,39 @@
+from sqlmodel import SQLModel, Field, Relationship
+from pydantic import EmailStr
+
+
+
+class UserBase(SQLModel):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    full_name: str | None = Field(default=None, max_length=255)
+
+
+class UserCreate(UserBase):
+    username: str = Field(unique=True)
+    password: str = Field(max_length=60)
+
+    
+
+class UserRegister(SQLModel):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    full_name: str | None = Field(default=None, max_length=255)
+    username: str = Field(unique=True)
+    password: str = Field(max_length=60)
+
+
+
+class UserPublic(UserBase):
+    id: int
+    username: str = Field(unique=True)
+
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(unique=True)
+    hashed_password: str = Field(max_length=60)
+
+
+
+    
+    
